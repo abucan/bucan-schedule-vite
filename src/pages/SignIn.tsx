e7b1c122b21cbbac9955047b163c2edf3be13344
Ante Bucan <ab8390@g.rit.edu>
2025-12-23 08:43:00 +0100

    Remove Convex authentication and related dependencies from the project. This includes deleting configuration files, auth-related functions, and updating package dependencies in package.json and package-lock.json.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,5 @@
 import { Logo } from '@/components/custom/Logo';
-import { SignInForm } from '@/components/custom/SignInForm';
+// import { SignInForm } from '@/components/custom/SignInForm';
 import { BackgroundBeams } from '@/components/ui/background-beams';
 
 function SignIn() {
@@ -8,7 +8,7 @@ function SignIn() {
       <section className='remove-scrollbar relative flex-1 overflow-y-auto px-[5%] my-auto z-10'>
         <div className='mx-auto flex size-full flex-col py-10 max-w-sm'>
           <Logo className='h-14 mb-12 self-center' />
-          <SignInForm />
+          {/* <SignInForm /> */}
           <div className='text-14-regular mt-14 flex items-center justify-center'>
             <p className='justify-items-end text-dark-600'>
               {new Date().getFullYear()} © All rights reserved.
